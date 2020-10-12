7071445e94fc5f8027bbaa9e3016e63ed1b76685
Joan Busquet <joan@joanbusquet.com>
2020-10-12 21:33:07 +0200

    Header develop

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/layout/layout.js"
+import Layout from "../components/layout/Layout"
 
 //Page components import
 import Hero from "../components/index/Hero"
